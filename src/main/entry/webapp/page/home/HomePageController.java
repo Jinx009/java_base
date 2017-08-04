2957ed20ea0d19a1c459563dbeb4ba5fc8a31f76
Jinx <jinxlovejinx@vip.qq.com>
2017-08-04 16:39:17 +0800

    delete some code

--- src/main/entry/webapp/page/home/HomePageController.java
+++ src/main/entry/webapp/page/home/HomePageController.java
@@ -6,53 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/home/p")
 public class HomePageController {
-
-	/**
-	 * 数据图表
-	 * @return
-	 */
-	@RequestMapping(path = "/lot")
-	public String lot(){
-		return "/home/pro/lot";
-	}
-	
-	/**
-	 * 地理位置
-	 * @return
-	 */
-	@RequestMapping(path = "/location")
-	public String location(){
-		return "/home/pro/location";
-	}
-	
-
-	
-	/**
-	 * 蓝牙设备
-	 * @return
-	 */
-	@RequestMapping(path = "/blue")
-	public String blue(){
-		return "/home/pro/blue";
-	}
-	
-	/**
-	 * 蓝牙设备
-	 * @return
-	 */
-	@RequestMapping(path = "/bikes")
-	public String bikes(){
-		return "/home/pro/bikes";
-	}
-	
-	/**
-	 * 示意图
-	 * @return
-	 */
-	@RequestMapping(path = "/view")
-	public String view(){
-		return "/home/pro/view";
-	}
 	
 	/**
 	 * pos机列表
