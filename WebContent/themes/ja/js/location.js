a9e086aae0560111dafadd629528122187ec21ef
Jinx <jinxlovejinx@vip.qq.com>
2017-10-25 17:37:03 +0800

    update

--- WebContent/themes/ja/js/location.js
+++ WebContent/themes/ja/js/location.js
@@ -6,19 +6,11 @@ function _setMap() {
 	for ( var i in _data) {
 		var _item = _data[i];
 		var _htmlStr = '';
-		if ('x' == _item._type) {
-			_htmlStr = '<img id="'
-					+ _item.lot
-					+ '"  src="/themes/img/A-car.png" width="30px;" style="position: absolute;top: '
-					+ _item._top + 'px;left: ' + _item._left
-					+ 'px;" class="none"  />';
-		} else {
-			_htmlStr = '<img id="'
-					+ _item.lot
-					+ '" src="/themes/img/A-car-v.png" height="30px;" style="position: absolute;top: '
-					+ _item._top + 'px;left: ' + _item._left
-					+ 'px;"  class="none"  />';
-		}
+		_htmlStr = '<img id="'
+				+ _item.lot
+				+ '"  src="/themes/img/A-car.png" width="20px;" style="position: absolute;top: '
+				+ _item._top + 'px;left: ' + _item._left
+				+ 'px;" class=" car none"  />';
 		$('.location').append(_htmlStr);
 	}
 	run();
@@ -27,7 +19,7 @@ function _setMap() {
 
 		run();
 
-	}, 5000);
+	}, 10000);
 }
 function run() {
 	$.get('/interface/ja/view?areaId=17', function(res) {
--- WebContent/themes/ja/js/location_data.js
+++ WebContent/themes/ja/js/location_data.js
@@ -1,282 +1,139 @@
 var _data = [ {
-	_left : 95,
-	_top : 154,
-	lot : '1',
+	_left : 50,
+	_top : 362,
+	lot : '26',
 	_type : 'x'
 }, {
-	_left : 129,
-	_top : 154,
-	lot : '2',
+	_left : 68,
+	_top : 355,
+	lot : '25',
 	_type : 'x'
 }, {
-	_left : 161,
-	_top : 154,
-	lot : '3',
+	_left : 86,
+	_top : 348,
+	lot : '24',
 	_type : 'x'
 }, {
-	_left : 195,
-	_top : 154,
-	lot : '4',
+	_left : 266,
+	_top : 277,
+	lot : '23',
 	_type : 'x'
 }, {
-	_left : 228,
-	_top : 154,
-	lot : '5',
+	_left : 286,
+	_top : 269,
+	lot : '22',
 	_type : 'x'
 },
 // 上右
 {
-	_left : 289,
-	_top : 154,
-	lot : '6',
+	_left : 304,
+	_top : 262,
+	lot : '21',
 	_type : 'x'
 }, {
 	_left : 322,
-	_top : 154,
-	lot : '7',
+	_top : 255,
+	lot : '20',
 	_type : 'x'
 }, {
-	_left : 355,
-	_top : 154,
-	lot : '8',
+	_left : 340,
+	_top : 248,
+	lot : '19',
 	_type : 'x'
 }, {
-	_left : 386,
-	_top : 154,
-	lot : '9',
+	_left : 358,
+	_top : 241,
+	lot : '18',
 	_type : 'x'
 }, {
-	_left : 418,
-	_top : 154,
-	lot : '10',
+	_left : 377,
+	_top : 234,
+	lot : '17',
 	_type : 'x'
 },
 // 二行
 {
-	_left : 95,
-	_top : 311,
-	lot : '11',
-	_type : 'x'
-}, {
-	_left : 129,
-	_top : 311,
-	lot : '12',
-	_type : 'x'
-}, {
-	_left : 161,
-	_top : 311,
-	lot : '13',
-	_type : 'x'
-}, {
-	_left : 195,
-	_top : 311,
-	lot : '14',
-	_type : 'x'
-}, {
-	_left : 227,
-	_top : 311,
-	lot : '15',
-	_type : 'x'
-},{
-	_left : 260,
-	_top : 311,
+	_left : 394,
+	_top : 227,
 	lot : '16',
 	_type : 'x'
 }, {
-	_left : 293,
-	_top : 311,
-	lot : '17',
+	_left : 412,
+	_top : 220,
+	lot : '15',
 	_type : 'x'
 }, {
-	_left : 325,
-	_top : 311,
-	lot : 'A18',
+	_left : 431,
+	_top : 212,
+	lot : '14',
+	_type : 'x'
+},
+{
+	_left : 622,
+	_top : 137,
+	lot : '13',
 	_type : 'x'
 }, {
-	_left : 357,
-	_top : 311,
-	lot : 'A19',
+	_left : 643,
+	_top : 129,
+	lot : '12',
+	_type : 'x'
+},{
+	_left : 694,
+	_top : 108,
+	lot : '11',
+	_type : 'x'
+}, {
+	_left : 713,
+	_top : 101,
+	lot : '10',
+	_type : 'x'
+}, {
+	_left : 731,
+	_top : 94,
+	lot : '9',
 	_type : 'x'
 },
 //第三行
 {
-	_left : 528,
-	_top : 430,
-	lot : 'A36',
+	_left : 748,
+	_top : 87,
+	lot : '8',
 	_type : 'y'
 },{
-	_left : 543,
-	_top : 430,
-	lot : '37',
+	_left : 765,
+	_top : 80,
+	lot : '7',
 	_type : 'y'
 },{
-	_left : 558,
-	_top : 430,
-	lot : '38',
+	_left : 783,
+	_top : 73,
+	lot : '6',
 	_type : 'y'
 },{
-	_left : 578,
-	_top : 430,
-	lot : '39',
+	_left : 800,
+	_top : 66,
+	lot : '5',
 	_type : 'y'
 },{
-	_left : 593,
-	_top : 430,
-	lot : '40',
+	_left : 818,
+	_top : 59,
+	lot : '4',
 	_type : 'y'
 },{
-	_left : 608,
-	_top : 430,
-	lot : '41',
+	_left : 837,
+	_top : 52,
+	lot : '3',
 	_type : 'y'
 },{
-	_left : 628,
-	_top : 430,
-	lot : '42',
+	_left : 854,
+	_top : 45,
+	lot : '2',
 	_type : 'y'
 },{
-	_left : 643,
-	_top : 430,
-	lot : '43',
+	_left : 874,
+	_top : 38,
+	lot : '1',
 	_type : 'y'
-},{
-	_left : 658,
-	_top : 430,
-	lot : '44',
-	_type : 'y'
-},{
-	_left : 676,
-	_top : 430,
-	lot : '45',
-	_type : 'y'
-},{
-	_left : 692,
-	_top : 430,
-	lot : '46',
-	_type : 'y'
-},{
-	_left : 707,
-	_top : 430,
-	lot : '47',
-	_type : 'y'
-},{
-	_left : 727,
-	_top : 430,
-	lot : '48',
-	_type : 'y'
-},{
-	_left : 742,
-	_top : 430,
-	lot : '49',
-	_type : 'y'
-},{
-	_left : 757,
-	_top : 430,
-	lot : '50',
-	_type : 'y'
-},{
-	_left : 775,
-	_top : 430,
-	lot : '51',
-	_type : 'y'
-},{
-	_left : 791,
-	_top : 430,
-	lot : '52',
-	_type : 'y'
-},{
-	_left : 806,
-	_top : 430,
-	lot : '53',
-	_type : 'y'
-},{
-	_left : 821,
-	_top : 430,
-	lot : '54',
-	_type : 'y'
-},
-//第四行
-{
-	_left : 97,
-	_top : 657,
-	lot : '20',
-	_type : 'x'
-},{
-	_left : 130,
-	_top : 657,
-	lot : '21',
-	_type : 'x'
-},{
-	_left : 162,
-	_top : 657,
-	lot : '22',
-	_type : 'x'
-},{
-	_left : 195,
-	_top : 657,
-	lot : '23',
-	_type : 'x'
-},{
-	_left : 226,
-	_top : 657,
-	lot : '24',
-	_type : 'x'
-},{
-	_left : 270,
-	_top : 657,
-	lot : 'A25',
-	_type : 'x'
-},{
-	_left : 303,
-	_top : 657,
-	lot : '26',
-	_type : 'x'
-},{
-	_left : 334,
-	_top : 657,
-	lot : '27',
-	_type : 'x'
-},{
-	_left : 366,
-	_top : 657,
-	lot : '28',
-	_type : 'x'
-},{
-	_left : 399,
-	_top : 657,
-	lot : '29',
-	_type : 'x'
-},
-//第五行
-{
-	_left : 2,
-	_top : 741,
-	lot : '30',
-	_type : 'x'
-},{
-	_left : 2,
-	_top : 757,
-	lot : '31',
-	_type : 'x'
-},{
-	_left : 2,
-	_top : 773,
-	lot : '32',
-	_type : 'x'
-},{
-	_left : 2,
-	_top : 790,
-	lot : '33',
-	_type : 'x'
-},{
-	_left : 2,
-	_top : 806,
-	lot : '34',
-	_type : 'x'
-},{
-	_left : 2,
-	_top : 822,
-	lot : '35',
-	_type : 'x'
-},
+}
 ];
