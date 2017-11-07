925034ea4ad70425d089999e3f12221721c02b6c
Jinx <jinxlovejinx@vip.qq.com>
2017-11-07 15:19:00 +0800

    justice

--- src/main/entry/webapp/task/GuodongSensorTask.java
+++ src/main/entry/webapp/task/GuodongSensorTask.java
@@ -1,4 +1,6 @@
 package main.entry.webapp.task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,11 +10,12 @@ import org.springframework.stereotype.Component;
 @Lazy(value=false)
 public class GuodongSensorTask {
 
+	private static final Logger logger = LoggerFactory.getLogger(GuodongSensorTask.class);
 	
 	
 	@Scheduled(fixedRate = 1000 * 10,initialDelay = 1000)
 	public void init(){
-		System.out.println("start job ... ... ");
+		logger.warn("start job ... ... ");
      }
 	
 }
