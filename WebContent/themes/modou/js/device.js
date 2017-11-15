4cc4e58e44f0f47abc664e9957eaa00cb37cbcd8
Jinx <jinxlovejinx@vip.qq.com>
2017-11-15 16:16:40 +0800

    防止误操作

--- WebContent/themes/modou/js/device.js
+++ WebContent/themes/modou/js/device.js
@@ -1,19 +1,20 @@
-$(function(){
+$(function() {
 	$.ajax({
-		url:'/interface/wenzhou_modou/device',
-		type:'get',
-		dataType:'json',
-		success:function(res){
-			for(var i in res.data.result){
+		url : '/interface/wenzhou_modou/device',
+		type : 'get',
+		dataType : 'json',
+		success : function(res) {
+			for ( var i in res.data.result) {
 				var _sensor = res.data.result[i];
-				if(''!=_sensor.bluetooth&&_sensor.bluetooth!=null){
+				if ('' != _sensor.bluetooth && _sensor.bluetooth != null) {
 					$.ajax({
-						url:'/common/bluetooth/findByUuid?uuid='+_sensor.bluetooth,
-						type:'get',
-						data:'json',
-						async:false,
-						success:function(resp){
-							if(resp.data!=null){
+						url : '/common/bluetooth/findByUuid?uuid='
+								+ _sensor.bluetooth,
+						type : 'get',
+						data : 'json',
+						async : false,
+						success : function(resp) {
+							if (resp.data != null) {
 								res.data.result[i].uid = resp.data.carNo;
 							}
 						}
@@ -21,41 +22,48 @@ $(function(){
 				}
 			}
 			new Vue({
-				el:'#data',
-				data:{
-					datas:res.data.result
+				el : '#data',
+				data : {
+					datas : res.data.result
 				}
 			})
 		}
 	})
 })
-function goJob(_element){
+function goJob(_element) {
 	var _target = $(_element).attr('target');
 	var _mac = $(_element).attr('mac');
-	$.ajax({
-		url:'/common/job/find?serviceName=modou',
-		type:'get',
-		dataType:'json',
-		success:function(res){
-			if('200'==res.code){
-				layer.alert('尚有未完成任务！');
-			}else{
-				$.ajax({
-					url:'/common/job/save/cfgsensor',
-					type:'post',
-					data:'target='+_target+'&mac='+_mac+'&serviceName=modou',
-					dataType:'json',
-					success:function(resp){
-						if('200'==resp.code){
-							layer.alert('正在校准中...',function(){
-								location.href = '/thmems/modou/job.html';
-							})
-						}else{
-							layer.alert(resp.msg);
+	layer.confirm('您确定要校准' + mac + '吗？', {
+		btn : [ '确定', '取消' ]
+	}, function() {
+		$.ajax({
+			url : '/common/job/find?serviceName=modou',
+			type : 'get',
+			dataType : 'json',
+			success : function(res) {
+				if ('200' == res.code) {
+					layer.alert('尚有未完成任务！');
+				} else {
+					$.ajax({
+						url : '/common/job/save/cfgsensor',
+						type : 'post',
+						data : 'target=' + _target + '&mac=' + _mac
+								+ '&serviceName=modou',
+						dataType : 'json',
+						success : function(resp) {
+							if ('200' == resp.code) {
+								layer.alert('正在校准中...', function() {
+									location.href = '/themes/modou/job.html';
+								})
+							} else {
+								layer.alert(resp.msg);
+							}
 						}
-					}
-				})
+					})
+				}
 			}
-		}
-	})
+		})
+	}, function() {
+		layer.closeAll();
+	});
 }
--- WebContent/themes/modou/js/job.js
+++ WebContent/themes/modou/js/job.js
@@ -4,6 +4,9 @@ $(function(){
 		type:'get',
 		dataType:'json',
 		success:function(res){
+			for(var i in res.data){
+				res.data[i].createTime = formatDateTime(res.data[i].createTime);
+			}
 			new Vue({
 				el:'#data',
 				data:{
@@ -13,3 +16,18 @@ $(function(){
 		}
 	})
 })
+function formatDateTime(inputTime) {    
+    var date = new Date(inputTime);  
+    var y = date.getFullYear();    
+    var m = date.getMonth() + 1;    
+    m = m < 10 ? ('0' + m) : m;    
+    var d = date.getDate();    
+    d = d < 10 ? ('0' + d) : d;    
+    var h = date.getHours();  
+    h = h < 10 ? ('0' + h) : h;  
+    var minute = date.getMinutes();  
+    var second = date.getSeconds();  
+    minute = minute < 10 ? ('0' + minute) : minute;    
+    second = second < 10 ? ('0' + second) : second;   
+    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;    
+};  
