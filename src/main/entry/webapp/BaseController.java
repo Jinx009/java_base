bc6591ab27cfafb3108765a9b3fb678eaabb1bdc
Jinx <jinxlovejinx@vip.qq.com>
2017-11-17 09:28:26 +0800

    justice will be done

--- src/main/entry/webapp/BaseController.java
+++ src/main/entry/webapp/BaseController.java
@@ -1,56 +1,23 @@
 package main.entry.webapp;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import database.models.WebTokenFactory;
 import database.models.home.HomeResource;
 import database.models.home.HomeUser;
-import service.basicFunctions.WebTokenFactoryService;
 import utils.ip.IPUtil;
 import utils.model.HomeConfigConstant;
-import utils.wechat.WechatData;
-import utils.wechat.WechatJSSign;
 
 @Controller
 public class BaseController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
-	
-	@Autowired
-	private WebTokenFactoryService webTokenFactoryService;
-
-	/**
-	 * 微信分享jsapi
-	 * @param request
-	 */
-	public void getJsApi(HttpServletRequest request){
-		String url = request.getRequestURL().toString();
-		String queryString = request.getQueryString();
-		try {
-			if (null != queryString && !"".equals(queryString)) 
-				url = url + "?" + queryString;
-			WebTokenFactory webTokenFactory = webTokenFactoryService.getByTypeAndId(WechatData.APP_ID,1);
-			Map<String, String> ret;
-			ret = WechatJSSign.createSign(webTokenFactory.getTokenValue(),url, WechatData.APP_ID, WechatData.APP_SECRET);
-			request.setAttribute("appId", WechatData.APP_ID);
-			request.setAttribute("timestamp", ret.get("timestamp").toString());
-			request.setAttribute("nonceStr", ret.get("nonceStr").toString());
-			request.setAttribute("signature", ret.get("signature").toString());
-			request.setAttribute("desc","");
-		} catch (Exception e) {
-			logger.error("[error:{}]",e);
-		} 
-	}
-	
 	/**
 	 * 获取客户端ip
 	 * @param request
