9c9ab28a77d6b7a86c4fbb23ad30efcc2e114c2b
Jinx <jinxlovejinx@vip.qq.com>
2018-01-17 18:04:16 +0800

    justice

--- WebContent/themes/js/home/order/detail.js
+++ WebContent/themes/js/home/order/detail.js
@@ -1,102 +1,178 @@
 $(function() {
-	var _d1 = 80, _d2 = 120;
-	var _d3 = 80, _d4 = 120;
-	var _d5 = 80, _d6 = 120;
-	$('#money').text(_d1);
-	$('#money_t').text(_d2);
-	$('#times').text(_d3);
-	$('#times_t').text(_d4);
-	$('#time').text(_d5);
-	$('#time_t').text(_d6);
-	draw_1(_d1, _d2);
-	draw_2(_d3, _d4);
-	draw_3(_d5, _d6);
+	_getData();
+//	laydate.render({
+//	  elem: '#datepicker'
+//	  ,type: 'datetime'
+//	  ,range: true
+//	}); 
 	laydate.render({
 		elem : '#datepicker',
 		range : true
 	});
 })
-var myChart_1 = echarts.init(document.getElementById('draw_1'));
-var myChart_2 = echarts.init(document.getElementById('draw_2'));
-var myChart_3 = echarts.init(document.getElementById('draw_3'));
-function draw_1(_current, _total) {
-	var _p = parseInt(_current / _total * 100);
-	var option = {
-		tooltip : {
-			formatter : "涉及金额：(" + _current + "元)</br>总金额：(" + _total
-					+ "元)</br>占比：{c}%"
-		},
-		toolbox : {
-			feature : {
-				restore : {},
-				saveAsImage : {}
-			}
-		},
-		series : [ {
-			name : '金额占比',
-			type : 'gauge',
-			detail : {
-				formatter : '{value}%'
-			},
-			data : [ {
-				value : _p,
-				name : '\n金额占比'
-			} ]
-		} ]
-	};
-	myChart_1.setOption(option, true);
+var myChart = echarts.init(document.getElementById('draw_1'));
+var myChart2 = echarts.init(document.getElementById('draw_2'));
+var myChart3 = echarts.init(document.getElementById('draw_3'));
+function _getData(){
+	var time = $('#datepicker').val();
+	var timestamp = (new Date()).valueOf();
+	var time2 = timestamp-60*60*24*1000;
+	var _beginTime = toDate(time2),_endTime = toDate(timestamp);
+	if(time!=null&&time!=''){
+		_beginTime = time.split(' - ')[0];
+		_endTime = time.split(' - ')[1];
+	}
+	_draw_1('');
+	_draw_2('');
+	_draw_3('');
+//	var params = 'beginTime='+_beginTime+'&endTime='+_endTime;
+//	$.ajax({
+//		url:'/home/d/mofang/order/statistics',
+//		type:'post',
+//		data:params,
+//		dataType:'json',
+//		success:function(res){
+//			_draw(res.data);
+//		}
+//	})
 }
-function draw_2(_current, _total) {
-	var _p = parseInt(_current / _total * 100);
-	var option = {
-		tooltip : {
-			formatter : "涉及次数：(" + _current + ")</br>总次数：(" + _total
-					+ ")</br>占比：{c}%"
-		},
-		toolbox : {
-			feature : {
-				restore : {},
-				saveAsImage : {}
-			}
-		},
-		series : [ {
-			name : '次数占比',
-			type : 'gauge',
-			detail : {
-				formatter : '{value}%'
-			},
-			data : [ {
-				value : _p,
-				name : '\n次数占比'
-			} ]
-		} ]
-	};
-	myChart_2.setOption(option, true);
+function _darw(_d){
+	_draw_1();
 }
-function draw_3(_current, _total) {
-	var _p = parseInt(_current / _total * 100);
-	var option = {
-		tooltip : {
-			formatter : "涉及分钟：(" + _current + ")</br>总分钟：(" + _total
-					+ ")</br>占比：{c}%"
-		},
-		toolbox : {
-			feature : {
-				restore : {},
-				saveAsImage : {}
-			}
-		},
-		series : [ {
-			name : '时长占比',
-			type : 'gauge',
-			detail : {
-				formatter : '{value}%'
-			},
-			data : [ {
-				value : _p,
-				name : '\n时长占比'
-			} ]
-		} ]
-	};
-	myChart_3.setOption(option, true);
+function _draw_1(_d){
+	option = {
+		    title: {
+		        text: '',
+		        subtext: '',
+		        left: 'center'
+		    },
+		    tooltip : {
+		        trigger: 'item',
+		        formatter: "{a} <br/>{b} : {c} ({d}%)"
+		    },
+		    legend: {
+		        // orient: 'vertical',
+		        // top: 'middle',
+		        bottom: 10,
+		        left: 'center',
+		        data: ['已支付', '在停','不需支付','逃单']
+		    },
+		    series : [
+		        {
+		            type: 'pie',
+		            radius : '65%',
+		            center: ['50%', '50%'],
+		            selectedMode: 'single',
+		            data:[
+		          
+		                {value:535, name: '已支付'},
+		                {value:510, name: '在停'},
+		                {value:634, name: '不需支付'},
+		                {value:735, name: '逃单'}
+		            ],
+		            itemStyle: {
+		                emphasis: {
+		                    shadowBlur: 10,
+		                    shadowOffsetX: 0,
+		                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+		                }
+		            }
+		        }
+		    ]
+		};
+	   myChart.setOption(option,true);
 }
+function _draw_2(_d){
+	option1 = {
+		    title: {
+		        text: '',
+		        subtext: '',
+		        left: 'center'
+		    },
+		    tooltip : {
+		        trigger: 'item',
+		        formatter: "{a} <br/>{b} : {c} ({d}%)"
+		    },
+		    legend: {
+		        // orient: 'vertical',
+		        // top: 'middle',
+		        bottom: 10,
+		        left: 'center',
+		        data: ['已支付', '在停','不需支付','逃单']
+		    },
+		    series : [
+		        {
+		            type: 'pie',
+		            radius : '65%',
+		            center: ['50%', '50%'],
+		            selectedMode: 'single',
+		            data:[
+		          
+		                {value:535, name: '已支付'},
+		                {value:510, name: '在停'},
+		                {value:634, name: '不需支付'},
+		                {value:735, name: '逃单'}
+		            ],
+		            itemStyle: {
+		                emphasis: {
+		                    shadowBlur: 10,
+		                    shadowOffsetX: 0,
+		                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+		                }
+		            }
+		        }
+		    ]
+		};
+	   myChart2.setOption(option1,true);
+}
+function _draw_3(_d){
+	option2 = {
+		    title: {
+		        text: '',
+		        subtext: '',
+		        left: 'center'
+		    },
+		    tooltip : {
+		        trigger: 'item',
+		        formatter: "{a} <br/>{b} : {c} ({d}%)"
+		    },
+		    legend: {
+		        // orient: 'vertical',
+		        // top: 'middle',
+		        bottom: 10,
+		        left: 'center',
+		        data: ['已支付', '在停','不需支付','逃单']
+		    },
+		    series : [
+		        {
+		            type: 'pie',
+		            radius : '65%',
+		            center: ['50%', '50%'],
+		            selectedMode: 'single',
+		            data:[
+		          
+		                {value:535, name: '已支付'},
+		                {value:510, name: '在停'},
+		                {value:634, name: '不需支付'},
+		                {value:735, name: '逃单'}
+		            ],
+		            itemStyle: {
+		                emphasis: {
+		                    shadowBlur: 10,
+		                    shadowOffsetX: 0,
+		                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+		                }
+		            }
+		        }
+		    ]
+		};
+	   myChart3.setOption(option,true);
+}
+var option = {};
+var option1 = {};
+var option2 = {};
+
+
+
+
+
