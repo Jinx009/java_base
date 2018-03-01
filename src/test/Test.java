d98b4aabdc4a3e28684579010003a8d22c7668ff
Jinx <jinxlovejinx@vip.qq.com>
2018-03-01 10:51:16 +0800

    x

--- src/test/Test.java
+++ src/test/Test.java
@@ -12,57 +12,34 @@ import java.util.List;
 
 public class Test {
 
+	private static double EARTH_RADIUS = 6378.137;//地球半径
+	private static double rad(double d)
+	{
+	   return d * Math.PI / 180.0;
+	} 
+
+	public static double getDistance(double lat1, double lng1, double lat2, double lng2)
+	{
+	   double radLat1 = rad(lat1);
+	   double radLat2 = rad(lat2);
+	   double a = radLat1 - radLat2;
+	   double b = rad(lng1) - rad(lng2);
+
+	   double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
+	    Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
+	   s = s * EARTH_RADIUS;
+	   s = Math.round(s * 10000) / 10000;
+	   return s;
+	}
+	
+	
 	public static void main(String[] args) {
-//		try {
-//		    String pathname = "/Users/jinx/Downloads/heart_log/heart_log_1107_0001170829000019.txt"; 
-//            File filename = new File(pathname); 
-//            if(filename.exists()){
-//            	 InputStreamReader reader = new InputStreamReader(  
-//                         new FileInputStream(filename)); 
-//                 BufferedReader br = new BufferedReader(reader); 
-//                 StringBuilder result = new StringBuilder();
-//                 String s = null;
-//                 while((s = br.readLine())!=null){//使用readLine方法，一次读一行
-//                     result.append(System.lineSeparator()+s);
-//                 }
-//                 br.close();  
-//                 System.out.println(result.toString());
-//            }else{
-//            	System.out.println("cuck");
-//            }
-//           
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		List<String> s = new ArrayList<String>();
-		s.add("1");
-		s.add("1");
-		s.add("1");
-		s.add("1");
-		s.add("1");
-		System.out.println(PPrinter.pformat(s));
-		
-		
-		
-//		String pathname = "/Users/jinx/Downloads/files/xxxx";
-//		File fileName = new File(pathname);
-//		if (fileName.exists()) {
-//			System.out.println(getLog(fileName));
-//		} else{
-//			System.out.println("xx");
-//		}
-//		File file=new File("/Users/jinx/Downloads");
-//        for(File temp:file.listFiles()){
-//            if(temp.isFile()){
-//                System.out.println(temp.toString());
-//            }
-//            
-//        }
+		System.out.println(getDistance(116.454071,39.978437,116.444585,39.983247));
 	}
 	
 	
 	
-	private static String getLog(File fileName){
+	public static String getLog(File fileName){
 		try {
 			InputStreamReader reader = new InputStreamReader(new FileInputStream(fileName));
 			BufferedReader br = new BufferedReader(reader);
