af6bd87f284bd54ebccf312c1764e9dcccc2aa83
Jinx <jinxlovejinx@vip.qq.com>
2018-07-26 17:40:29 +0800

    change data save method

--- src/main/entry/webapp/data/qj/QingjiaoDataController.java
+++ src/main/entry/webapp/data/qj/QingjiaoDataController.java
@@ -73,9 +73,13 @@ public class QingjiaoDataController extends BaseController {
 				qjDevice.setType(type);
 				qjDevice.setCreateTime(new Date());
 				qjDevice.setXType(Integer.valueOf(dataArray[10] + dataArray[11]));
-				qjDevice.setXValue(getData(dataArray[12], dataArray[12] + dataArray[13] + dataArray[14] + dataArray[15]));
+				String xValue = getData(dataArray[12], dataArray[12] + dataArray[13] + dataArray[14] + dataArray[15]);
+				double _x = Double.valueOf(xValue)-Double.valueOf(qjDevice.getXValue());
+				qjDevice.setXValue(String.valueOf(_x));
 				qjDevice.setYType(Integer.valueOf(dataArray[16] + dataArray[17]));
-				qjDevice.setYValue(getData(dataArray[18], dataArray[18] + dataArray[19] + dataArray[20] + dataArray[21]));
+				String yValue = getData(dataArray[18], dataArray[18] + dataArray[19] + dataArray[20] + dataArray[21]);
+				double _y = Double.valueOf(yValue)-Double.valueOf(qjDevice.getYValue());
+				qjDevice.setYValue(String.valueOf(_y));
 				qjDevice.setVoltage(getData(dataArray[22], dataArray[22] + dataArray[23] + dataArray[24] + dataArray[25]));
 				qjDeviceService.update(qjDevice);
 			}
