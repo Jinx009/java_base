26a9e1fedb115dfa4ba5d8cda1bba14ff445e37a
Jinx <jinxlovejinx@vip.qq.com>
2018-08-08 17:39:41 +0800

    add function

--- src/main/entry/webapp/data/TelcomCotroller.java
+++ src/main/entry/webapp/data/TelcomCotroller.java
@@ -188,6 +188,8 @@ public class TelcomCotroller extends BaseController{
 						HttpUtils.get("http://zhxf.gdzxkj.net:8003/api/devices_get_single_info?sign="+MD5Util.toMD5(_s).toLowerCase()+"&"+_s);
 					}else if(ioTCloudDevice.getLocalIp()!=null&&ioTCloudDevice.getLocalIp().equals("QJ")){
 						HttpUtils.get("http://app.zhanway.com/home/cloud/qj/push?data="+tModel.getData());
+					}else if(ioTCloudDevice.getLocalIp()!=null&&ioTCloudDevice.getLocalIp().equals("QJ_ZHANWAY")){
+						HttpUtils.get("http://app.zhanway.com/home/cloud/qj/zhanway/push?data="+tModel.getData());
 					}else{
 						send(tModel.getData(),ioTCloudDevice.getUdpIp(),ioTCloudDevice.getUdpPort());
 					}
