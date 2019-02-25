676a91ffed11652696bdb0ab33e015d8ebc3c9e6
Jinx <jinxlovejinx@vip.qq.com>
2019-02-25 15:19:07 +0800

    TEST

--- src/utils/ip/IPUtil.java
+++ src/utils/ip/IPUtil.java
@@ -1,6 +1,9 @@
 package utils.ip;
 
 
+import java.util.Iterator;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class IPUtil {
@@ -28,5 +31,11 @@ public class IPUtil {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println(System.getenv("mysqlUrl"));
+		  Map<String, String> map = System.getenv();
+	        for(Iterator<String> itr = map.keySet().iterator();itr.hasNext();){
+	            String key = itr.next();
+	            System.out.println(key + "=" + map.get(key));
+	        }   
 	}
 }
