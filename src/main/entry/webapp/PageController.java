b78515717a4ee214eeba850182aaf850e58fc99b
Jinx <jinxlovejinx@vip.qq.com>
2019-08-29 13:49:04 +0800

    fix

--- src/main/entry/webapp/PageController.java
+++ src/main/entry/webapp/PageController.java
@@ -2,9 +2,7 @@ package main.entry.webapp;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletResponse;
