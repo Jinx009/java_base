81fc8c6e7ee2d47f8f47c2b6ca2de383b93bdea6
Jinx <jinxlovejinx@vip.qq.com>
2019-10-23 12:44:07 +0800

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -97,38 +97,105 @@ public class Test {
 	
 	
 	public static void main(String[] args) {
+//		try {
+//			String data = "0001180614000002488e3d0b00080108002c001b2a2f00";
+//			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            Date date = new Date();
+//            File f = new File("/apps/logs/sensor_status/"+sdf.format(date)+".txt");
+//            File fileParent = f.getParentFile();
+//			if (!fileParent.exists()) {
+//				fileParent.mkdirs();
+//				f.createNewFile();
+//			}
+//            FileWriter fw = new FileWriter(f, true);
+//            PrintWriter pw = new PrintWriter(fw);
+//            pw.println(data+"     "+sdf1.format(date));
+//            pw.flush();
+//            fw.flush();
+//            pw.close();
+//            fw.close();
+//            String av = data.substring(24, 26);
+//            Integer avalable = Integer.valueOf(av);
+//            String mode = data.substring(26, 28);
+//            String dif1 = data.substring(32, 34);
+//            String dif2 = data.substring(30, 32);
+//            String zdif1 = data.substring(36, 38);
+//            String zdif2 = data.substring(34, 36);
+//			System.out.println(av);
+//			System.out.println(Long.parseLong(mode, 16));
+//			System.out.println(getDataBase(data.substring(32, 33), dif1+dif2));
+//			System.out.println(getDataBase(data.substring(36, 37), zdif1+zdif2));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 		try {
-			String data = "0001180614000002488e3d0b00080108002c001b2a2f00";
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = new Date();
-            File f = new File("/apps/logs/sensor_status/"+sdf.format(date)+".txt");
-            File fileParent = f.getParentFile();
-			if (!fileParent.exists()) {
-				fileParent.mkdirs();
-				f.createNewFile();
-			}
-            FileWriter fw = new FileWriter(f, true);
-            PrintWriter pw = new PrintWriter(fw);
-            pw.println(data+"     "+sdf1.format(date));
-            pw.flush();
-            fw.flush();
-            pw.close();
-            fw.close();
-            String av = data.substring(24, 26);
-            Integer avalable = Integer.valueOf(av);
-            String mode = data.substring(26, 28);
-            String dif1 = data.substring(32, 34);
-            String dif2 = data.substring(30, 32);
-            String zdif1 = data.substring(36, 38);
-            String zdif2 = data.substring(34, 36);
-			System.out.println(av);
-			System.out.println(Long.parseLong(mode, 16));
-			System.out.println(getDataBase(data.substring(32, 34), dif1+dif2));
-			System.out.println(getDataBase(data.substring(36, 37), zdif1+zdif2));
+			String data = "000118061400000248093e21af000107322e313033008b0d12120a28082d0b126416009afa0100f2fc4f001c00";
+			String dif1 = data.substring(26, 28);
+			String dif2 = data.substring(24, 26);
+			String dif = getDataBase(data.substring(26, 27), dif1+dif2);
+			System.out.println("dif:"+dif);
+			String av = data.substring(28, 30);
+			Integer avalable = Integer.valueOf(av);
+			System.out.println(avalable); 
+			String bat1 = data.substring(46, 48);
+			String bat2 = data.substring(44, 46);
+			String bat = getDataBase(data.substring(44, 45), bat1+bat2);
+			System.out.println("bat:"+bat); 
+			String soft = convertHexToString(data.substring(32, 44));
+			System.out.println("soft:"+soft); 
+			String lt = String.valueOf(Integer.parseInt( data.substring(48, 50),16));
+			System.out.println("lt:"+lt); 
+			String ht = String.valueOf(Integer.parseInt( data.substring(50, 52),16));
+			System.out.println("ht:"+ht); 
+			String wl = String.valueOf(Integer.parseInt( data.substring(52, 54),16));
+			System.out.println("wl:"+wl); 
+			String wh = String.valueOf(Integer.parseInt( data.substring(54, 56),16));
+			System.out.println("wh"+wh); 
+			String ang =  String.valueOf(Integer.parseInt( data.substring(56, 58),16));
+			System.out.println("ang:"+ang); 
+			String wt =  String.valueOf(Integer.parseInt( data.substring(58, 60),16));
+			System.out.println("wt:"+wt); 
+			String rssi =  String.valueOf(Integer.parseInt( data.substring(66, 68),16));
+			System.out.println("rssi:"+rssi); 
+			String sdi =  String.valueOf(Integer.parseInt( data.substring(60, 62),16));
+			System.out.println("sdi:"+sdi); 
+			String sdo =  String.valueOf(Integer.parseInt( data.substring(62, 64),16));
+			System.out.println("sdo:"+sdo); 
+			String fdi =  String.valueOf(Integer.parseInt( data.substring(64, 66),16));
+			System.out.println("fdi:"+fdi); 
+			String pci1 = data.substring(88, 90);
+			String pci2 = data.substring(86, 88);
+			String pci = getDataBase(data.substring(88, 89), pci1+pci2);
+			System.out.println("pci:"+pci); 
+			String rsrp1 = data.substring(80, 82);
+			String rsrp2 = data.substring(78, 80);
+			String rsrp =  getDataBase(data.substring(80, 81), rsrp1+rsrp2);
+			System.out.println("rsrp:"+rsrp); 
+			String snr1 = data.substring(84,86);
+			String snr2 = data.substring(82, 84);
+			String snr =   getDataBase(data.substring(84, 85), snr1+snr2);
+			System.out.println("snr:"+snr); 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	public static String convertHexToString(String hex) {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder temp = new StringBuilder();
+		// 49204c6f7665204a617661 split into two characters 49, 20, 4c...
+		for (int i = 0; i < hex.length() - 1; i += 2) {
+			// grab the hex in pairs
+			String output = hex.substring(i, (i + 2));
+			// convert hex to decimal
+			int decimal = Integer.parseInt(output, 16);
+			// convert the decimal to character
+			sb.append((char) decimal);
+			temp.append(decimal);
+		}
+		return sb.toString();
+	}
+
 	
 }
