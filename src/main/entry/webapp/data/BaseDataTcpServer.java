d2ec89ca9d5732c4a3be76c8a9df20da20c4429d
Jinx <jinxlovejinx@vip.qq.com>
2020-03-02 13:54:45 +0800

    fix

--- src/main/entry/webapp/data/BaseDataTcpServer.java
+++ src/main/entry/webapp/data/BaseDataTcpServer.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * 监测院数据转发
+ * 
  * @author Jinx
  *
  */
@@ -31,56 +32,59 @@ public class BaseDataTcpServer extends Thread {
 				// 从请求队列中取出一个连接
 				Socket client = server.accept();
 				// 处理这次连接
-//				new HandlerThread(client);
+				// new HandlerThread(client);
 				try {
-					log.warn("base data tcp server client :{} ,{}", client.getInetAddress().getLocalHost(), "conn success");
-					while (true) {
-						if (client != null) {
-							BufferedInputStream bufferedInputStream = new BufferedInputStream(client.getInputStream());
-							if (bufferedInputStream.available() > 0) {
-								byte[] receive = new byte[256];
-								int read = bufferedInputStream.read(receive);
-								dataArray = receive;
-								log.warn("server rec data：{}", read);
-							}
+					log.warn("base data tcp server client :{} ,{}", client.getInetAddress().getLocalHost(),
+							"conn success");
+					if (client != null) {
+						BufferedInputStream bufferedInputStream = new BufferedInputStream(client.getInputStream());
+						if (bufferedInputStream.available() > 0) {
+							byte[] receive = new byte[256];
+							int read = bufferedInputStream.read(receive);
+							dataArray = receive;
+							log.warn("server rec data：{}", read);
 						}
+						client.shutdownInput();
+						client.close();
 					}
 				} catch (Exception e) {
 					log.error("base data tcp server thread error:{}: ", e.getMessage());
-				} 
+				}
 			}
 		} catch (Exception e) {
 			log.error("base data tcp server error:{}", e);
 		}
 	}
 
-//	private class HandlerThread implements Runnable {
-//		private Socket socket;
-//
-//		public HandlerThread(Socket client) {
-//			this.socket = client;
-//			new Thread(this).start();
-//		}
-//
-//		@SuppressWarnings("static-access")
-//		public void run() {
-//			try {
-//				log.warn("base data tcp server client :{} ,{}", socket.getInetAddress().getLocalHost(), "conn success");
-//				while (true) {
-//					if (socket != null) {
-//						BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-//						if (bufferedInputStream.available() > 0) {
-//							byte[] receive = new byte[256];
-//							int read = bufferedInputStream.read(receive);
-//							dataArray = receive;
-//							log.warn("server rec data：{}", read);
-//						}
-//					}
-//				}
-//			} catch (Exception e) {
-//				log.error("base data tcp server thread error:{}: ", e.getMessage());
-//			} 
-//		}
-//	}
+	// private class HandlerThread implements Runnable {
+	// private Socket socket;
+	//
+	// public HandlerThread(Socket client) {
+	// this.socket = client;
+	// new Thread(this).start();
+	// }
+	//
+	// @SuppressWarnings("static-access")
+	// public void run() {
+	// try {
+	// log.warn("base data tcp server client :{} ,{}",
+	// socket.getInetAddress().getLocalHost(), "conn success");
+	// while (true) {
+	// if (socket != null) {
+	// BufferedInputStream bufferedInputStream = new
+	// BufferedInputStream(socket.getInputStream());
+	// if (bufferedInputStream.available() > 0) {
+	// byte[] receive = new byte[256];
+	// int read = bufferedInputStream.read(receive);
+	// dataArray = receive;
+	// log.warn("server rec data：{}", read);
+	// }
+	// }
+	// }
+	// } catch (Exception e) {
+	// log.error("base data tcp server thread error:{}: ", e.getMessage());
+	// }
+	// }
+	// }
 
 }
