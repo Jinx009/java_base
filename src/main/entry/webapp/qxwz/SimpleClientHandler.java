0801818c62d5f0347c1264c35cd1b6406c016019
Jinx <jinxlovejinx@vip.qq.com>
2020-03-11 13:44:37 +0800

    fix

--- src/main/entry/webapp/qxwz/SimpleClientHandler.java
+++ src/main/entry/webapp/qxwz/SimpleClientHandler.java
@@ -138,7 +138,7 @@ public class SimpleClientHandler extends ChannelInboundHandlerAdapter{
 		
 		try {
 //			Socket s = new Socket("139.224.237.198", 6666);
-			Socket s = new Socket("139.224.237.198", 6666);
+			Socket s = new Socket("139.196.205.157", 6666);
 			
 			OutputStream outputStream = s.getOutputStream();
 			outputStream.write(req);
