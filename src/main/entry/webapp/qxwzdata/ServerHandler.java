d9496ee5a59d036fe9c4b0b6eb43ceb53fce3f8e
Jinx <jinxlovejinx@vip.qq.com>
2020-03-30 16:21:26 +0800

    fix

--- src/main/entry/webapp/qxwzdata/ServerHandler.java
+++ src/main/entry/webapp/qxwzdata/ServerHandler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import utils.HttpUtils;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -22,7 +24,35 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 	 */
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		HttpUtils.get("http://127.0.0.1:8080/d/socketSave?status=conn&ip="+ctx.channel().remoteAddress().toString().split(":")[0]+"_6666");
 		NettyConfig.group.add(ctx.channel());
+		new Thread() {
+			public void run() {
+				String str = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+				byte[] data = new byte[] {};
+				int i = 10000;
+				while (true) {
+					try {
+						String s = "";
+						if(i<13600) {
+							s = i+str+i;
+							
+						}else {
+							i = 10000;
+							s = i+str+i;
+						}
+						data = s.getBytes();
+						ByteBuf pingMessage = ctx.alloc().buffer(data.length);
+						pingMessage.writeBytes(data);
+						ctx.writeAndFlush(pingMessage);
+						Thread.sleep(1000);
+						i++;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
 	}
 
 	/**
@@ -30,6 +60,7 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 	 */
 	@Override
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		HttpUtils.get("http://127.0.0.1:8080/d/socketSave?status=close&ip="+ctx.channel().remoteAddress().toString().split(":")[0]+"_6666");
 		NettyConfig.group.remove(ctx.channel());
 	}
 
@@ -109,6 +140,14 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 		} catch (Exception e) {
 			log.error("error:{}", e);
 		}
+	}
+	
+	public static void main(String[] args) {
+		String str = "";
+		for(int i = 0;i<1024;i++) {
+			str+= "0";
+		}
+		System.out.println(str);
 	}
 
 }
