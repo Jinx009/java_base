6f982f7a5631880efa8a475f7ba1e31eb05a2fd8
Jinx <jinxlovejinx@vip.qq.com>
2020-04-27 15:45:56 +0800

    fix

---
+++ src/main/entry/webapp/data/wxapp/GnssDataController.java
@@ -0,0 +1,46 @@
+package main.entry.webapp.data.wxapp;
+
+import javax.servlet.http.HttpUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import main.entry.webapp.BaseController;
+import main.entry.webapp.data.qj.QingjiaoDataController;
+import utils.Resp;
+
+@Controller
+@RequestMapping(value = "/home/cloud/gnss")
+public class GnssDataController extends BaseController{
+	
+	private static final Logger log = LoggerFactory.getLogger(GnssDataController.class);
+	
+
+	@RequestMapping(path = "/device")
+	@ResponseBody
+	public Resp<?> device() {
+		Resp<?> resp = new Resp<>(false);
+		try {
+			return new Resp<>(utils.HttpUtils.get("http://139.196.205.157:8080/d/openDeviceData"));
+		} catch (Exception e) {
+			log.error("error:{}", e);
+		}
+		return resp;
+	}
+	
+	@RequestMapping(path = "/log")
+	@ResponseBody
+	public Resp<?> log(String mac) {
+		Resp<?> resp = new Resp<>(false);
+		try {
+			return new Resp<>(utils.HttpUtils.get("http://139.196.205.157:8080/d/openLogData?mac="+mac));
+		} catch (Exception e) {
+			log.error("error:{}", e);
+		}
+		return resp;
+	}
+	
+}
