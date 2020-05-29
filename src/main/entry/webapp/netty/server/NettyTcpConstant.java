2be3fc505323ff5c2a4dbc886e77fff85aeaac03
Jinx <Jinx@Jinx>
2020-05-29 20:38:44 +0800

    fix

--- src/main/entry/webapp/netty/server/NettyTcpConstant.java
+++ src/main/entry/webapp/netty/server/NettyTcpConstant.java
@@ -1,10 +1,10 @@
 package main.entry.webapp.netty.server;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NettyTcpConstant {
 
-	public static Map<String, Object> map = new HashMap<String, Object>();
+	public static Map<String, Object> map = new ConcurrentHashMap <String, Object>();
 	
 }
