68e9c6fd9c4bd8e3e750cc3f60bb1e058af0ad27
Jinx <jinxlovejinx@vip.qq.com>
2020-09-15 14:23:46 +0800

    fix

---
+++ src/main/entry/webapp/data/wxapp/SensorDataController.java
@@ -0,0 +1,28 @@
+package main.entry.webapp.data.wxapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import main.entry.webapp.BaseController;
+import utils.Resp;
+
+@Controller
+@RequestMapping(value = "/home/cloud/sendor")
+public class SensorDataController extends BaseController{
+
+	private Logger log = LoggerFactory.getLogger(SensorDataController.class);
+	
+	public Resp<?> save(){
+		Resp<?> resp = new Resp<>(false);
+		try {
+			
+		} catch (Exception e) {
+			log.error("e:{}",e);
+		}
+		return resp;
+	}
+	
+}
