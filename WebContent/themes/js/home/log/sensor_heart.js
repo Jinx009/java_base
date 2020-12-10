f05a544b4ea624f1898fd1ece285fba16ecaf3e0
Jinx <jinxlovejinx@vip.qq.com>
2020-12-10 13:15:06 +0800

    雷达地磁

--- WebContent/themes/js/home/log/sensor_heart.js
+++ WebContent/themes/js/home/log/sensor_heart.js
@@ -12,6 +12,7 @@ function _getData(){
 	var _data = {};
 	_data.mac = mac;
 	_data.dateStr = dateStr;
+	$('#data').hide();
 	$.ajax({
 		url:'/d/log_sensor/heart/1_0',
 		dataType:'json',
@@ -19,6 +20,9 @@ function _getData(){
 		contentType:'application/json;charSet=utf8',
 		type:'post',
 		success:function(res){
+			if(res.data!=null&&res.data!=''){
+				$('#data').show();
+			}
 			for(var i in res.data){
 				res.data[i].createTime = toDateTime(res.data[i].createTime);
 			}
